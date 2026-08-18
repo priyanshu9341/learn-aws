981dc69a1d58f06f7a323bb72f541f8b6caf4f77
priyanshu9341 <priyanshukumar79037@gmail.com>
2026-08-19 01:11:08 +0530

    new

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/health", (req, res) => {
 })
 
 app.get("/", (req, res) => {
-    return res.status(200).json({message: "hello priyanshu👍"})
+    return res.status(200).json({message: "hello priyanshu v2👍"})
 })
 
 app.listen(port, ()=>{
