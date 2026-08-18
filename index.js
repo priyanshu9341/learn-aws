93ea2789a5d21534a2794310bd99c3ac4cbf502c
priyanshu9341 <priyanshukumar79037@gmail.com>
2026-08-19 01:12:46 +0530

    Update greeting message in root endpoint

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/health", (req, res) => {
 })
 
 app.get("/", (req, res) => {
-    return res.status(200).json({message: "hello priyanshu v2👍"})
+    return res.status(200).json({message: "hello priyanshu kumar v2👍"})
 })
 
 app.listen(port, ()=>{
